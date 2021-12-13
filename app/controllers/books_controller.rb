1ae3f1c52a8f9fa4f20ad2325cda4eafbe3a7c79
eri-Q <eri.hatanaka8@gmail.com>
2021-12-13 12:01:06 +0000

    [Add]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,6 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
-  def show
-    @books = Book.find(params[:id])
-  end
-
   def new
     @book = Book.new
   end
@@ -18,7 +14,24 @@ class BooksController < ApplicationController
     redirect_to books_path(book.id)
   end
 
+  def show
+    @book = Book.find(params[:id])
+  end
+
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
